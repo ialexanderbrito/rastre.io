5b5539eb1b2f79dd6609e8e29f8000d1ad1e6529
ialexanderbrito <xanderzinho26@gmail.com>
2022-01-13 12:27:12 -0300

    👽 Adicionado permissões API

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 const apiCorreios = axios.create({
   baseURL: `https://proxyapp.correios.com.br/v1/sro-rastro/`,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'X-Requested-With': 'XMLHttpRequest',
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:36.0) Gecko/20100101 Firefox/36.0',
+  },
 });
 
 const apiCep = axios.create({
