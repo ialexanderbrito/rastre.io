925a784ea8682647a8f48fbcc2df8286c2d073e3
ialexanderbrito <xanderzinho26@gmail.com>
2022-01-13 10:57:14 -0300

    🚚 Adicionado rota de CEP

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { Rastreio } from 'pages/Rastreio'
+import { Cep } from 'pages/Cep';
+import { Rastreio } from 'pages/Rastreio';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Rastreio />} />
+      <Route path="/cep" element={<Cep />} />
     </Routes>
-  )
+  );
 }
