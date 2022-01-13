c1a61b552569d4b3439278078b55b7145c84f8d1
ialexanderbrito <xanderzinho26@gmail.com>
2022-01-13 13:20:09 -0300

    🐛 Fix imports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
