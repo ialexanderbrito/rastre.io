a58f864d3c02ddc6f7b202a2e0b2492c6370fcac
ialexanderbrito <xanderzinho26@gmail.com>
2022-01-13 11:49:18 -0300

    ✨ Adicionado providers App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
+
+import { CepProvider } from 'context/Cep';
+import { RastreamentoProvider } from 'context/Rastreamento';
 import { MainRoutes } from 'routes';
 import 'styles/global.scss';
 
 export function App() {
   return (
     <BrowserRouter>
-      <MainRoutes />
+      <RastreamentoProvider>
+        <CepProvider>
+          <MainRoutes />
+          <Toaster
+            containerStyle={{
+              top: 65,
+            }}
+          />
+        </CepProvider>
+      </RastreamentoProvider>
     </BrowserRouter>
   );
 }
