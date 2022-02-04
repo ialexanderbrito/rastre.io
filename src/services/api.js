8de6572f01fe56341c8b0bee7edc2c134ab6749e
ialexanderbrito <xanderzinho26@gmail.com>
2022-02-04 19:08:19 -0300

    👽️ Correção CORS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiCorreios = axios.create({
-  baseURL: `https://cors-anywhere.herokuapp.com/https://proxyapp.correios.com.br/v1/sro-rastro/`,
+  baseURL: `https://thingproxy.freeboard.io/fetch/https://proxyapp.correios.com.br/v1/sro-rastro/`,
 });
 
 const apiCep = axios.create({
