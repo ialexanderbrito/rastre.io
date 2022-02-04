50af728cfa5cebde7f37458760b995953cf375ac
ialexanderbrito <xanderzinho26@gmail.com>
2022-02-04 19:38:38 -0300

    👽️ Bug fix CORS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiCorreios = axios.create({
-  baseURL: `https://thingproxy.freeboard.io/fetch/https://proxyapp.correios.com.br/v1/sro-rastro/`,
+  baseURL: `https://cors-all-proxy.herokuapp.com/fetch/https://proxyapp.correios.com.br/v1/sro-rastro/`,
 });
 
 const apiCep = axios.create({
