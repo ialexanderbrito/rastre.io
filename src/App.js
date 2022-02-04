4bd2619104862050eac3983c7269249182ef0b03
ialexanderbrito <xanderzinho26@gmail.com>
2022-02-04 11:09:13 -0300

    ✨ Adicionado context no App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
 
+import { AuthProvider } from 'context/Auth';
 import { CepProvider } from 'context/Cep';
 import { RastreamentoProvider } from 'context/Rastreamento';
 import { ThemeProvider } from 'context/Theme';
@@ -11,16 +12,18 @@ export function App() {
   return (
     <BrowserRouter>
       <ThemeProvider>
-        <RastreamentoProvider>
-          <CepProvider>
-            <MainRoutes />
-            <Toaster
-              containerStyle={{
-                top: 65,
-              }}
-            />
-          </CepProvider>
-        </RastreamentoProvider>
+        <AuthProvider>
+          <RastreamentoProvider>
+            <CepProvider>
+              <MainRoutes />
+              <Toaster
+                containerStyle={{
+                  top: 65,
+                }}
+              />
+            </CepProvider>
+          </RastreamentoProvider>
+        </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
   );
