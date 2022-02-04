fed2da351c42b201533792bea4a72ceb7b383665
ialexanderbrito <xanderzinho26@gmail.com>
2022-02-04 11:18:32 -0300

    🔥 Removido .env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const supabaseServKey = process.env.REACT_APP_SUPABASE_SERV_KEY;
+const supabaseUrl = 'https://agejhjpwmddgycuxdfss.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzkyNzE1MywiZXhwIjoxOTU5NTAzMTUzfQ.vz4FepWJmhZ8dt7NQR_5uJXPLYnH6UsdGchJNlxgm8Q';
+const supabaseServKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzOTI3MTUzLCJleHAiOjE5NTk1MDMxNTN9.LRsCDoCxgMHgBhf5gZiTVQ4KsD-Vh5WqdaqoUTVHqiE';
 
 export const supabase = createClient(
   supabaseUrl,
