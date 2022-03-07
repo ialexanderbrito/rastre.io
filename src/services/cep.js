cfe2ad413e33ba28071627d9a78179273a9e5478
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-07 13:16:46 -0300

    🔥 Removido arquivos .js

--- src/services/cep.js
+++
@@ -1,6 +0,0 @@
-import { apiCep } from './api';
-
-export async function getCep(cep) {
-  const { data } = await apiCep.get(`/${cep}/json`);
-  return data;
-}
--- src/services/rastreio.js
+++
@@ -1,6 +0,0 @@
-import { apiCorreios } from './api';
-
-export async function getRastreio(trackingCode) {
-  const { data, status } = await apiCorreios.get(`/${trackingCode}`);
-  return { data, status };
-}
