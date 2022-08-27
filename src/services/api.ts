f92a98d201f51568fc182cd23d149a60411f3331
Alexander <xanderzinho26@gmail.com>
2022-08-26 22:31:17 -0300

    👽️ Atualizado CORS link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiCorreios = axios.create({
-  baseURL: `https://cors-all-proxy.herokuapp.com/fetch/https://proxyapp.correios.com.br/v1/sro-rastro/`,
+  baseURL: `https://cors.ialexanderbrito.dev/fetch/https://proxyapp.correios.com.br/v1/sro-rastro/`,
 });
 
 const apiCep = axios.create({
